a7fb2b57f0aeda10ac42153d9a5399bc0112f108
ChristofferHall <christoffer.hall@brightstraining.com>
2022-11-08 16:29:09 +0100

    This is a clone of my GitHub project again

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! This is a clone");
+        System.out.println("Hello world! This is a changed clone");
 
     }
 }
