68bffde71fc1ea8cf600d9353e029489b7dcb1aa
ChristofferHall <christoffer.hall@brightstraining.com>
2022-11-09 09:55:17 +0100

    1. Initial add 1.1 Added Change 1 to output 1.2 Added Change 2 to output 1.3 ? 1.4 Cleanup 1.5 Clone again
    2.0 This is a new branch

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.println("Hello world! This is a clone of a clone");
         System.out.println("Hello world! This is a changed clone");
+        System.out.println("This is a new feature (and branch)");
 
     }
 }
